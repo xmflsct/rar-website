4d12fd1764dd6e8ede2cb4ef333f1fd7f5b782aa
Zhiyuan Zheng <me@xmflsct.com>
2019-10-17 18:41:03 +0200

    User two pass rendering
    [](https://github.com/gatsbyjs/gatsby/issues/17914)

--- src/components/sidebar/opening-hours.js
+++ src/components/sidebar/opening-hours.js
@@ -1,90 +1,139 @@
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql } from "gatsby";
 
-function checkStatus(data) {
-  if (
-    Date.now() >= Date.parse(data.closedDates.start) &&
-    Date.now() <= Date.parse(data.closedDates.end)
-  ) {
-    return <span className="shop-badge shop-close">Closed for holiday</span>;
-  } else {
-    if (data.openingHours.nodes[new Date().getDay()].open) {
-      var currentTime = new Date();
-      var almostOpen = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
-          .hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
-          .minute,
-        0
+class OpeningHours extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClient: false };
+  }
+
+  render() {
+    if (
+      Date.now() >= Date.parse(this.props.closedDates.start) &&
+      Date.now() <= Date.parse(this.props.closedDates.end)
+    ) {
+      return (
+        <span className="shop-badge shop-close" key={this.state.isClient}>
+          Closed for holiday
+        </span>
       );
-      var open = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].open[0].hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].open[0].minute,
-        0
-      );
-      var close = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].close[0].hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].close[0].minute,
-        0
-      );
-      var lastOrder = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
-          .hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
-          .minute,
-        0
-      );
-      if (currentTime >= almostOpen && currentTime < open) {
-        return <span className="shop-badge shop-open-soon">Open soon</span>;
-      } else if (currentTime >= open && currentTime < lastOrder) {
-        return <span className="shop-badge shop-open">Open now</span>;
-      } else if (currentTime >= lastOrder && currentTime < close) {
-        return <span className="shop-badge shop-last-order">Last order</span>;
-      } else {
-        return <span className="shop-badge shop-close">Closed</span>;
-      }
     } else {
-      return <span className="shop-badge shop-close">Closed for today</span>;
+      if (this.props.openingHours.nodes[new Date().getDay()].open) {
+        var currentTime = new Date();
+        var almostOpen = new Date().setHours(
+          this.props.openingHours.nodes[new Date().getDay()].hours[0]
+            .almost_open[0].hour,
+          this.props.openingHours.nodes[new Date().getDay()].hours[0]
+            .almost_open[0].minute,
+          0
+        );
+        var open = new Date().setHours(
+          this.props.openingHours.nodes[new Date().getDay()].hours[0].open[0]
+            .hour,
+          this.props.openingHours.nodes[new Date().getDay()].hours[0].open[0]
+            .minute,
+          0
+        );
+        var close = new Date().setHours(
+          this.props.openingHours.nodes[new Date().getDay()].hours[0].close[0]
+            .hour,
+          this.props.openingHours.nodes[new Date().getDay()].hours[0].close[0]
+            .minute,
+          0
+        );
+        var lastOrder = new Date().setHours(
+          this.props.openingHours.nodes[new Date().getDay()].hours[0]
+            .last_order[0].hour,
+          this.props.openingHours.nodes[new Date().getDay()].hours[0]
+            .last_order[0].minute,
+          0
+        );
+        if (currentTime >= almostOpen && currentTime < open) {
+          return (
+            <span
+              className="shop-badge shop-open-soon"
+              key={this.state.isClient}
+            >
+              Open soon
+            </span>
+          );
+        } else if (currentTime >= open && currentTime < lastOrder) {
+          return (
+            <span className="shop-badge shop-open" key={this.state.isClient}>
+              Open now
+            </span>
+          );
+        } else if (currentTime >= lastOrder && currentTime < close) {
+          return (
+            <span
+              className="shop-badge shop-last-order"
+              key={this.state.isClient}
+            >
+              Last order
+            </span>
+          );
+        } else {
+          return (
+            <span className="shop-badge shop-close" key={this.state.isClient}>
+              Closed
+            </span>
+          );
+        }
+      } else {
+        return (
+          <span className="shop-badge shop-close" key={this.state.isClient}>
+            Closed for today
+          </span>
+        );
+      }
     }
+  }
+
+  componentDidMount() {
+    this.setState({ isClient: true });
   }
 }
 
-const OpeningHours = () => {
-  const data = useStaticQuery(graphql`
-    {
-      openingHours: allOpeningHoursJson(sort: { order: ASC, fields: day }) {
-        nodes {
-          day
-          open
-          hours {
-            almost_open {
-              hour
-              minute
-            }
-            open {
-              hour
-              minute
-            }
-            close {
-              hour
-              minute
-            }
-            last_order {
-              hour
-              minute
+export default props => (
+  <StaticQuery
+    query={graphql`
+      query {
+        openingHours: allOpeningHoursJson(sort: { order: ASC, fields: day }) {
+          nodes {
+            day
+            open
+            hours {
+              almost_open {
+                hour
+                minute
+              }
+              open {
+                hour
+                minute
+              }
+              close {
+                hour
+                minute
+              }
+              last_order {
+                hour
+                minute
+              }
             }
           }
         }
+        closedDates: dataJson {
+          start
+          end
+        }
       }
-      closedDates: dataJson {
-        start
-        end
-      }
-    }
-  `);
-  return (
-    <div>{checkStatus(data)}</div>
-  )
-};
-
-export default OpeningHours;
+    `}
+    render={({ openingHours, closedDates }) => (
+      <OpeningHours
+        openingHours={openingHours}
+        closedDates={closedDates}
+        {...props}
+      />
+    )}
+  />
+);
