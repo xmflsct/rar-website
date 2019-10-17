4ff56830d826bbfa761f02aed7d9d27739bfe54f
Zhiyuan Zheng <me@xmflsct.com>
2019-10-17 11:26:38 +0200

    Try with a delay

--- src/components/sidebar/opening-hours.js
+++ src/components/sidebar/opening-hours.js
@@ -34,51 +34,53 @@ const OpeningHours = () => {
       }
     }
   `);
-  if (
-    Date.now() >= Date.parse(data.closedDates.start) &&
-    Date.now() <= Date.parse(data.closedDates.end)
-  ) {
-    return <span className="shop-badge shop-close">Closed for holiday</span>;
-  } else {
-    if (data.openingHours.nodes[new Date().getDay()].open) {
-      var currentTime = new Date();
-      var almostOpen = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
-          .hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
-          .minute,
-        0
-      );
-      var open = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].open[0].hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].open[0].minute,
-        0
-      );
-      var close = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].close[0].hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].close[0].minute,
-        0
-      );
-      var lastOrder = new Date().setHours(
-        data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
-          .hour,
-        data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
-          .minute,
-        0
-      );
-      if (currentTime >= almostOpen && currentTime < open) {
-        return <span className="shop-badge shop-open-soon">Open soon</span>;
-      } else if (currentTime >= open && currentTime < lastOrder) {
-        return <span className="shop-badge shop-open">Open now</span>;
-      } else if (currentTime >= lastOrder && currentTime < close) {
-        return <span className="shop-badge shop-last-order">Last order</span>;
-      } else {
-        return <span className="shop-badge shop-close">Closed</span>;
-      }
+  setTimeout(function() {
+    if (
+      Date.now() >= Date.parse(data.closedDates.start) &&
+      Date.now() <= Date.parse(data.closedDates.end)
+    ) {
+      return <span className="shop-badge shop-close">Closed for holiday</span>;
     } else {
-      return <span className="shop-badge shop-close">Closed for today</span>;
+      if (data.openingHours.nodes[new Date().getDay()].open) {
+        var currentTime = new Date();
+        var almostOpen = new Date().setHours(
+          data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
+            .hour,
+          data.openingHours.nodes[new Date().getDay()].hours[0].almost_open[0]
+            .minute,
+          0
+        );
+        var open = new Date().setHours(
+          data.openingHours.nodes[new Date().getDay()].hours[0].open[0].hour,
+          data.openingHours.nodes[new Date().getDay()].hours[0].open[0].minute,
+          0
+        );
+        var close = new Date().setHours(
+          data.openingHours.nodes[new Date().getDay()].hours[0].close[0].hour,
+          data.openingHours.nodes[new Date().getDay()].hours[0].close[0].minute,
+          0
+        );
+        var lastOrder = new Date().setHours(
+          data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
+            .hour,
+          data.openingHours.nodes[new Date().getDay()].hours[0].last_order[0]
+            .minute,
+          0
+        );
+        if (currentTime >= almostOpen && currentTime < open) {
+          return <span className="shop-badge shop-open-soon">Open soon</span>;
+        } else if (currentTime >= open && currentTime < lastOrder) {
+          return <span className="shop-badge shop-open">Open now</span>;
+        } else if (currentTime >= lastOrder && currentTime < close) {
+          return <span className="shop-badge shop-last-order">Last order</span>;
+        } else {
+          return <span className="shop-badge shop-close">Closed</span>;
+        }
+      } else {
+        return <span className="shop-badge shop-close">Closed for today</span>;
+      }
     }
-  }
+  }, 500);
 };
 
 export default OpeningHours;
