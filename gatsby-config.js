3ae9f6278cd4c97c8abac1ee88c22e1cbe68f4eb
Zhiyuan Zheng <me@xmflsct.com>
2020-02-07 00:23:34 +0100

    Fix gatsby-plugin-sitemap error
    siteUrl should be used

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     title: "Round&Round Rotterdam",
     description:
       "Round&Round, Handmade Cakes&Crafts. Leading a cross-cultural lifestyle that relaxes you with an adult sense of cuteness. 跨文化＋治愈系可爱感＋手作蛋糕&杂货＝R&R生活美学",
-    url: "https://roundandround.nl",
+    siteUrl: "https://roundandround.nl",
     image: "./static/favicon.png"
   },
   plugins: [
