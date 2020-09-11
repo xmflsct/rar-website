eee2cbdad2b5fc8506678820ddf404a14944c932
Zhiyuan Zheng <me@xmflsct.com>
2020-09-11 21:15:19 +0200

    Increase order amount

--- src/components/cake-order.jsx
+++ src/components/cake-order.jsx
@@ -39,6 +39,16 @@ const CakeOrder = ({ cake }) => {
         <option value={3}>3 × {unit}</option>
         <option value={4}>4 × {unit}</option>
         <option value={5}>5 × {unit}</option>
+        <option value={6}>6 × {unit}</option>
+        <option value={7}>7 × {unit}</option>
+        <option value={8}>8 × {unit}</option>
+        <option value={9}>9 × {unit}</option>
+        <option value={10}>10 × {unit}</option>
+        <option value={11}>11 × {unit}</option>
+        <option value={12}>12 × {unit}</option>
+        <option value={13}>13 × {unit}</option>
+        <option value={14}>14 × {unit}</option>
+        <option value={15}>15 × {unit}</option>
       </Form.Control>
     </Form.Group>
   )
