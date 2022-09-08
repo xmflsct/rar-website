3cdd0d2a1480f2c8fcf6c70cbdffbdb92139b324
xmflsct <me@xmflsct.com>
2022-09-08 12:28:04 +0000

    Update Sentry reporting

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,7 +5,7 @@ import { hydrate } from 'react-dom'
 
 Sentry.init({
   dsn: 'https://79b2ce77fbfc4511af541c7a4cf125d3@o389581.ingest.sentry.io/6617177',
-  tracesSampleRate: 0.5,
+  tracesSampleRate: 0.1,
   integrations: [
     new Sentry.BrowserTracing({
       routingInstrumentation: Sentry.remixRouterInstrumentation(
