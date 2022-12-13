a224bb4885bf74bf365fea03776841d52323a686
xmflsct <me@xmflsct.com>
2022-12-13 18:28:08 +0100

    Update index.tsx

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -49,7 +49,7 @@ export default () => {
         <h2 className='text-2xl mb-4'>[Shop Holiday]</h2>
         <div>
           <p className='mb-2'>Dear everyone,</p>
-          <p className='mb-2'>December 24th will be the last day we open in 2022.<br />Round & Round will be open only for order pick-up.<br />Matcha Next Door will be open normally from 12:00- 18:00 :)</p>
+          <p className='mb-2'>December 24th will be the last day we open in 2022.<br /> - Round & Round will be open only for order pick-up.<br /> - Matcha Next Door will be open normally from 12:00- 18:00 :)</p>
           <p className='mb-2'>From December 25th - January 03 both spaces will be closed. We will be back on January 4th Wednesday.</p>
           <p className='mb-2'>Merry Christmas & Happy New Year!</p>
           <p className='mb-2'>Love,<br />Team R&R</p>
