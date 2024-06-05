bc6490f6708e5b1e692125f55686e07750d21c41
xmflsct <me@xmflsct.com>
2024-06-05 23:15:28 +0200

    Test Sentry

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,6 +1,12 @@
 import { RemixBrowser } from '@remix-run/react'
+import * as Sentry from '@sentry/remix'
 import { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
+
+Sentry.init({
+  dsn: 'https://79b2ce77fbfc4511af541c7a4cf125d3@o389581.ingest.us.sentry.io/6617177',
+  tracesSampleRate: 1
+})
 
 startTransition(() => {
   hydrateRoot(
