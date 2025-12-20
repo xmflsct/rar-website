abec7f744405957c22c3df2c2bf519ad98c6f3ed
xmflsct <me@xmflsct.com>
2025-12-20 11:52:24 +0100

    Bump dependencies

--- remix.config.js
+++ remix.config.js
@@ -9,5 +9,12 @@ module.exports = {
   serverPlatform: 'neutral',
   serverDependenciesToBundle: 'all',
   serverMinify: true,
-  serverNodeBuiltinsPolyfill: {}
+  serverNodeBuiltinsPolyfill: {},
+  future: {
+    v3_fetcherPersist: true,
+    v3_lazyRouteDiscovery: true,
+    v3_relativeSplatPath: true,
+    v3_singleFetch: true,
+    v3_throwAbortReason: true
+  }
 }
