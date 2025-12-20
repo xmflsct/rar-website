06a86d8a0da1d3f57c58fa51b1354e0c77da352e
xmflsct <me@xmflsct.com>
2025-12-20 14:44:13 +0100

    Optimizations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{ts,tsx}'],
+  future: {
+    hoverOnlyWhenSupported: true
+  },
   theme: {
     fontFamily: {
       sans: ['fot-tsukuardgothic-std', 'sans-serif']
